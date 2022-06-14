6d6c454a46a0b1d8de3c5a184f145a9052fac6ec
Sam Burdick <burdick.52@osu.edu>
2022-06-14 14:56:16 -0400

    update script

--- anti_uav_yolo/convert_files.py
+++ anti_uav_yolo/convert_files.py
@@ -9,7 +9,7 @@ WIDTH = 1920
 HEIGHT = 1080
 
 # Convert video into series of pngs
-def save_frames(video_input_path):
+def get_frames(video_input_path):
     if "~" in video_input_path:
         video_input_path = os.path.expanduser(video_input_path)
     if not os.path.isfile(video_input_path):
@@ -23,18 +23,17 @@ def save_frames(video_input_path):
             break
         frames.append(frame)
     return frames
-        
 
-def convert_frame_data(jsonfilepath, imagedir, labeldir):
-    with open(jsonfilepath, "r") as jsonfile:
+def convert_frame_data(data_dir, imagedir, labeldir):
+    with open(data_dir + "/visible.json", "r") as jsonfile:
         j = json.loads(jsonfile.read())
-        assert len(j["exist"]) == len(j["gt_rect"]), "Invalid frame data inputs"
+        frames = get_frames(data_dir + "/visible.mp4")
+        assert len(j["exist"]) == len(j["gt_rect"]) == len(frames), "Invalid frame data inputs"
         idx = 0
         # get file name
-        filename = jsonfilepath.split("/")[-2]
-        for exists, bbox, frame in zip(j["exist"], j["gt_rect"], ):
-            bbox = [int(x) for x in bbox]
-            if exists == '1':
+        filename = data_dir.split("/")[-1]
+        for exists, bbox, frame in zip(j["exist"], j["gt_rect"], frames):
+            if exists == 1:
                 ctr_x = (bbox[0] + bbox[2] / 2) / WIDTH
                 ctr_y = (bbox[1] + bbox[3] / 2) / HEIGHT
                 bbox_width = bbox[2] / WIDTH
@@ -42,7 +41,7 @@ def convert_frame_data(jsonfilepath, imagedir, labeldir):
                 # Write frame data to file
                 with open(f"{labeldir}/{filename}_f{idx}.txt", "w") as datafile:
                     datafile.write(f"0 {ctr_x} {ctr_y} {bbox_width} {bbox_height}")
-                cv2.imwrite(f"{imagedir}/{idx}.jpg", frame)
+                cv2.imwrite(f"{imagedir}/{filename}_f{idx}.jpg", frame)
             idx += 1
 
 def test_bbox(image_file, bbox):
@@ -62,14 +61,11 @@ def _fail(msg):
     exit()
 
 if __name__ == '__main__':
-    # video = sys.argv[1]
-    # path = sys.argv[2]
-    # test_bbox("./tmp/0.jpg", [650,390,69,42])
-    # test_bbox("./tmp/1.jpg", [631,393,70,43])
-    with open("/Users/sam/Library/CloudStorage/OneDrive-TheOhioStateUniversity/Anti-UAV-RGBT/val/20190925_101846_1_4/visible.json", "r") as jfile:
-        j = json.loads(jfile.read())
-        idx = 0
-        for box in j["gt_rect"]:
-            test_bbox(f"./tmp/{idx}.jpg", box)
-            idx += 1
-    #save_frames(video, path)
+    basepath = sys.argv[1]
+    for st in ['test', 'train', 'val']:
+        imagedir = os.path.join("images", st)
+        labeldir = os.path.join("labels", st)
+        for subdir in os.listdir(os.path.join(basepath, st)):
+            print(f"Converting data for {subdir}")
+            convert_frame_data(os.path.join(basepath, st, subdir), imagedir, labeldir)
+
